ee87eab7edebdcc15d5b1cb0146d21ace156a9ae
Branislav <branislav@konstrukt.rs>
2020-08-17 12:57:30 +0200

    changed origin

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,35 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require(`path`)
+const { createFilePath } = require(`gatsby-source-filesystem`)
+const BlogPostTemplates = path.resolve("./src/templates/BlogPost.js")
 
-// You can delete this file if you're not using it
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+
+  return graphql(`
+    {
+      allWordpressPost {
+        edges {
+          node {
+            wordpress_id
+            slug
+          }
+        }
+      }
+    }
+  `).then(result => {
+    if (result.errors) {
+      throw result.errors
+    }
+    console.log(result)
+    const BlogPosts = result.data.allWordpressPost.edges
+    BlogPosts.forEach(post => {
+      createPage({
+        path: `/post/${post.node.slug}`,
+        component: BlogPostTemplates,
+        context: {
+          id: post.node.wordpress_id,
+        },
+      })
+    })
+  })
+}
